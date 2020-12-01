aaccaa77d148567ae7467dddac02638cc42cb78c
BiaoBiaoZhangZiyang <996478975@qq.com>
2020-12-01 19:07:02 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,19 @@
 int main(){
     char windowName[] = "Test";
     cv::Mat canvas = cv::Mat::ones(Height, Width, CV_8UC3);
-    cv::Mat logo_orignal = cv::imread("/home/zzy/Desktop/myVideoType/pics/zju_orignal.png", 1);
+    cv::Mat logo_orignal = cv::imread("/home/zzy/Desktop/myVideoType/pics/zju_white.png", 1);
+    cv::Mat grayLogo;
+    cv::cvtColor(logo_orignal, grayLogo, cv::COLOR_BGR2GRAY);
+    tools::showCurrentPics(windowName, grayLogo);
+
     std::cout<<logo_orignal.cols<<"\n";
     std::cout<<logo_orignal.rows<<"\n";
-    cv::Mat lookUpTable(1, 256, CV_8U);
+    // cv::Mat lookUpTable(1, 256, CV_8U);
     // 遍历？
     // uchar* p = lookUpTable.ptr();
     // for(int i = 0;i<256)
     tools::insertIntoRegion(canvas, logo_orignal, 0.5); 
     tools::showCurrentPics(windowName, canvas);
+    
     return 0;
 }
